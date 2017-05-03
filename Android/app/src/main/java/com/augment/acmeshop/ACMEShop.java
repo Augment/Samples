a745530e58558440beb37640d26e871271155895
Jerome Gangneux <jerome.gangneux@augment.com>
2017-05-03 11:33:30 +0200

    Android iOS - include AppId/AppSecret demo keys

--- Android/app/src/main/java/com/augment/acmeshop/ACMEShop.java
+++ Android/app/src/main/java/com/augment/acmeshop/ACMEShop.java
@@ -19,9 +19,9 @@ public class ACMEShop extends Application {
     public static String PARCELABLE_PRODUCT_TAG  = "com.augment.acme-shop.product";
 
     public static class Augment {
-        public static String AppId     = "Please replace this with your own Augment App Id";
-        public static String AppSecret = "Please replace this with your own Augment App Secret";
-        public static String Vuforia   = "Please replace this with your own Vuforia Key";
+        public static String AppId     = "357fee36746668573ceb2f5957c4869ee1a62a112639bac9b0fae43c7c431692";
+        public static String AppSecret = "80ae1420e164e0440d5329067bcdd953e9fa6c63b75c001c06d169a4f11268c5";
+        public static String Vuforia   = "ATQqCM7/////AAAAGXLs+GRi0UwXh0X+/qQL49dbZGym8kKo+iRtgC95tbJoCWjXXZihDl5pzxoca2JxLcYxBJ2pIeIE4dNcK0etMeb1746L7lq6vSFen43cS7P1P/HXjwHtUouV5Xus2U0F7WHUTKuO629jKFO13fBQczuY52UJcSEhsu9jHPMaupo5CpqQT3TFTQjlhzHhVXiVMEqq7RI+Edwh8TCSfGAbNRdbIELTfK+8YDYqwEHDbp62mFrs68YnCEQZDrpcLyC8WzFCVZtnUq3Cj3YBUfQ6gNnENYiuLf06gAAF/FcaF65VYveGRBbp3hpkqolX28bxPiUYNVknCSFXICPHciVntxF+rcHW5rrX7Cg/IVFGdNRF";
     }
 
     @Override
--- iOS/ACMEShop/ACMEShop.swift
+++ iOS/ACMEShop/ACMEShop.swift
@@ -26,8 +26,8 @@ struct ACMEShop {
      * </dict>
      */
     struct Augment {
-        static let AppId     = "Please replace this with your own Augment App Id"
-        static let AppSecret = "Please replace this with your own Augment App Secret"
-        static let Vuforia   = "Please replace this with your own Augment Vuforia Key"
+        static let AppId     = "357fee36746668573ceb2f5957c4869ee1a62a112639bac9b0fae43c7c431692"
+        static let AppSecret = "80ae1420e164e0440d5329067bcdd953e9fa6c63b75c001c06d169a4f11268c5"
+        static let Vuforia   = "ATQqCM7/////AAAAGXLs+GRi0UwXh0X+/qQL49dbZGym8kKo+iRtgC95tbJoCWjXXZihDl5pzxoca2JxLcYxBJ2pIeIE4dNcK0etMeb1746L7lq6vSFen43cS7P1P/HXjwHtUouV5Xus2U0F7WHUTKuO629jKFO13fBQczuY52UJcSEhsu9jHPMaupo5CpqQT3TFTQjlhzHhVXiVMEqq7RI+Edwh8TCSfGAbNRdbIELTfK+8YDYqwEHDbp62mFrs68YnCEQZDrpcLyC8WzFCVZtnUq3Cj3YBUfQ6gNnENYiuLf06gAAF/FcaF65VYveGRBbp3hpkqolX28bxPiUYNVknCSFXICPHciVntxF+rcHW5rrX7Cg/IVFGdNRF"
     }
 }
